f81e7eae69b7e406e68e6b9e1d417ef50f63b2d0
joella jose <joellajose@gmailcom>
2022-12-06 13:58:08 +0400

    Fixed.

--- broken_grades.py
+++ broken_grades.py
@@ -21,36 +21,36 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: ")) #set the variable to integer
 
-exam_3 = str(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: ")) #changed the variable type from str to int and corrected the variable name
 
-grades = [exam_one exam_two exam_three]
+grades = [exam_one, exam_two, exam_three]
 sum = 0
-for grade in grade:
-  sum = sum + grade
+for grade in range(len(grades)): #added the range funtion and set logic loop
+  sum = sum + grades[grade] #corrected the operation
 
-avg = sum / len(grdes)
+avg = sum // len(grades)
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90: #added colon
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
+    letter_grade = "C" #put correct quotation
 elif avg <= 69 and avg >= 65:
     letter_grade = "D"
-elif:
+else: #changed elif to else
     letter_grade = "F"
 
-for grade in grades:
-    print("Exam: " + str(grade))
+#for grade in grades:  removed the loop and corrected the syntax
+print(f'Exams: {grades[0]}, {grades[1]}, {grades[2]}') #getting each element the grades list
 
-    print("Average: " + str(avg))
+print("Average: " +str(avg))
 
-    print("Grade: " + letter_grade)
+print("Grade: " + letter_grade)
 
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade is "F": #corrected the variable name
+    print ("Student is failing.") #added brackets
 else:
-    print "Student is passing."
+    print ("Student is passing.") #added brackets
