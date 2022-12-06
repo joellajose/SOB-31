1cd33058102a997500846cb1e99b97a4f6ef90a5
joella jose <joellajose@gmailcom>
2022-12-06 14:00:43 +0400

    Fixed.

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?")#added missing quotation
+if greeting =="Arrr!": #coorected the synthax
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: #replaced elif by else
+	print("Greetings, hater of pirates!") #corrected indentation error
