b09dfba84fc0022702f3ac625d486585fc865228
joella jose <joellajose@gmailcom>
2022-12-06 14:10:13 +0400

    Fixed.

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -77,7 +77,7 @@ def convert_input_to_coordinate(user_input):
     return user_input - 1
 
 def switch_player(player):
-    if player = 1:
+    if player == 1: #added missing =
         return 2
     else:
         return 1
@@ -98,12 +98,12 @@ if __name__ == '__main__':
     while winner == 0 and moves_exist(game):
         print("Currently player: " + str(player))
         available = False
-        while not available
+        while not available: #added colon
             row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
             column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
-            available = check_space_empty(game, row)
+            available = check_space_empty(game, row,column) #added missing argument column
         game = add_piece(game, player, row, column)
         display_game(game)
         player = switch_player(player)
-#        winner = check_winner(game)
+        winner = check_winner(game) #uncommmentedmit
     display_winner(winner)
